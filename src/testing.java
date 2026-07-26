b9d6d10d1125e0f063606d4a41e702a3972d8b94
Rishabh kumar <princekumar987@icloud.com>
2026-07-26 23:37:06 +0530

    first jenkins test

---
+++ src/testing.java
@@ -0,0 +1,10 @@
+import java.time.LocalDateTime;
+
+public class testing {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("My Name is Rishabh Kumar");
+        System.out.println(LocalDateTime.now().toString());
+        System.out.println("Jenkins CI/CD");
+    }
+}
