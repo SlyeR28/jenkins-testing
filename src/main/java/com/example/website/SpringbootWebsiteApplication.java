990406ea66732ea9e373a20cd00c414d26b0c908
Rishabh kumar <princekumar987@icloud.com>
2026-07-27 11:56:29 +0530

    Coverted jar to war file

--- src/main/java/com/example/website/SpringbootWebsiteApplication.java
+++ src/main/java/com/example/website/SpringbootWebsiteApplication.java
@@ -2,9 +2,16 @@ package com.example.website;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootWebsiteApplication {
+public class SpringbootWebsiteApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringbootWebsiteApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootWebsiteApplication.class, args);
