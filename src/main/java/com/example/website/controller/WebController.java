0f6e4820eeb45c42097900a288f7be820c0d5043
Rishabh kumar <princekumar987@icloud.com>
2026-07-27 12:55:26 +0530

    " make some changes"

--- src/main/java/com/example/website/controller/WebController.java
+++ src/main/java/com/example/website/controller/WebController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@RequestMapping("/javawebapp")
 @Controller
 public class WebController {
 
